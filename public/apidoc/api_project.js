8c341d26061c645dee5d9b7712aaf137fb0ae71a
asier.zapata <asier.zapata@gmail.com>
2017-10-10 18:43:42 +0200

    Apidocjs integration

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "BE-Hackathon API documentation",
+  "version": "1.0.0",
+  "description": "BackEnd API of Hackathon project",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-10-10T16:40:37.840Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
--- src/entities/match/matchModel.js
+++ src/entities/match/matchModel.js
@@ -4,6 +4,34 @@ var Match = require('./matchSchema');
 /*                    Implementation                      */
 /* ====================================================== */
 
+/**
+ * @api {post} /match Adds a new match
+ * @apiGroup Matchs
+ * @apiParam {String[]} teams The teams of the match
+ * @apiParam {Date} time The date in JS format
+ * @apiParam {String} game Game of this match
+ * @apiParam {String} competition League or tournament where this match will take place
+ * @apiSuccess {Object} match The resultant object inserted in the db
+ * @apiSuccessExample {Object} Success
+ *    HTTP/1.1 200 OK
+ *    [{
+ *      "_id": "59dbbcf933f189109ccef174",
+ *      "game": "League of Legends",
+ *      "competition": "Worlds",
+ *      "__v": 0,
+ *      "time": "2017-10-09 20:16:25.754",
+ *      "teams": [
+ *          "LZ",
+ *          "GAM"
+ *      ]
+ *    }]
+ * @apiExample {curl} Example usage:
+ *      curl --request POST --url http://localhost/match/ --header 'content-type: application/x-www-form-urlencoded' --data 'teams=IMT&teams=FNC&game=League%20of%20Legends&competition=Worlds'
+ * @apiErrorExample {String} DB error:
+ *    HTTP/1.1 500 Internal Server Error
+ *      There was a problem adding this match.
+ */
+
 const createMatch = function (req, res) {
     console.log('Post at / of match_controller called');
     Match.create({
@@ -18,6 +46,16 @@ const createMatch = function (req, res) {
     });
 }
 
+/**
+ * @api {get} /match List of all matches
+ * @apiGroup Matchs
+ * @apiSuccess {Object[]} matches Array of all matches
+ * @apiExample {curl} Example usage:
+ *      curl --request GET --url http://localhost/match/
+ * @apiErrorExample {String} DB error:
+ *    HTTP/1.1 500 Internal Server Error
+ *      "There was a problem finding all matches."
+ */
 const getAllMatches = function (req, res) {
     console.log('Get at / of match_controller called');    
     Match.find({}, function (err,matches) {
@@ -26,6 +64,33 @@ const getAllMatches = function (req, res) {
     });
 }
 
+/**
+ * @api {get} /match/:id Obtain match by id
+ * @apiGroup Matchs
+ * @apiParam {String} id 
+ * @apiSuccess {Object} match 
+ * @apiExample {curl} Example usage:
+ *      curl --request GET --url http://localhost/match/59dbbcf933f189109ccef174
+ * @apiSuccessExample {Object} Success
+ *    HTTP/1.1 200 OK
+ *    [{
+ *      "_id": "59dbbcf933f189109ccef174",
+ *      "game": "League of Legends",
+ *      "competition": "Worlds",
+ *      "__v": 0,
+ *      "time": "2017-10-09 20:16:25.754",
+ *      "teams": [
+ *          "LZ",
+ *          "GAM"
+ *      ]
+ *    }]
+ * @apiErrorExample {String} DB error:
+ *    HTTP/1.1 500 Internal Server Error
+ *      "There was a problem finding the match with id :id."
+ * @apiErrorExample {String} Match error:
+ *    HTTP/1.1 404 Not Found
+ *      "Match not found."
+ */
 const getMatchById = function (req, res) {
     Match.findById(req.params.id, function (err, match) {
         if (err) return res.status(500).send("There was a problem finding the match with id "+req.params.id+".");
@@ -34,6 +99,21 @@ const getMatchById = function (req, res) {
     });
 }
 
+/**
+ * @api {delete} /match/:id Delete match by id
+ * @apiGroup Matchs
+ * @apiParam {String} id 
+ * @apiSuccess {String} confirmation 
+ * @apiExample {curl} Example usage:
+ *      curl --request DELETE --url http://localhost/match/59dbbd5233f189109ccef175
+ * @apiSuccessExample {Object} Success
+ *    HTTP/1.1 200 OK
+ *      "Match with id :id was deleted."
+ * @apiErrorExample {String} DB error:
+ *    HTTP/1.1 500 Internal Server Error
+ *      "There was a problem deleting the match."
+ */
+
 const deleteMatch = function (req, res) {
     Match.findByIdAndRemove(req.params.id, function (err, match) {
         if (err) return res.status(500).send("There was a problem deleting the match.");
@@ -41,6 +121,30 @@ const deleteMatch = function (req, res) {
     });
 }
 
+/**
+ * @api {put} /match/:id Update a match by id
+ * @apiGroup Matchs
+ * @apiParam {String} id 
+ * @apiSuccess {Object} match New match object updated 
+ * @apiExample {curl} Example usage:
+ *     curl --request PUT --url http://localhost/match/59dbbcf933f189109ccef174 --header 'content-type: application/x-www-form-urlencoded' --data 'teams=FNC&teams=GAM'
+ * @apiSuccessExample {Object} Success
+ *    HTTP/1.1 200 OK
+ *      [{
+ *      "_id": "59dbbcf933f189109ccef174",
+ *      "game": "League of Legends",
+ *      "competition": "Worlds",
+ *      "__v": 0,
+ *      "time": "2017-10-09 20:16:25.754",
+ *      "teams": [
+ *          "FNC",
+ *          "GAM"
+ *      ]
+ *    }]
+ * @apiErrorExample {String} DB error:
+ *    HTTP/1.1 500 Internal Server Error
+ *      "There was a problem deleting the match."
+ */
 const updateMatch = function (req, res) {  
     Match.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, match) {
         if (err) return res.status(500).send("There was a problem updating the match.");
