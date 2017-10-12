59d6fccc132f0fba91545ed4ec6b1c30babce833
xogne <asier.zapata@gmail.com>
2017-10-12 23:41:17 +0200

    first parser commit

---
+++ src/parser/league-of-legends/parserMatch.js
@@ -0,0 +1,23 @@
+/* ====================================================== */
+/*                         API                            */
+/* ====================================================== */
+
+const API = {
+    'parseNewMatches' : parseNewMatches,
+    'parseMatchDone' : parseMatchDone
+}
+
+module.exports = API;
+
+/* ====================================================== */
+/*                    Implementation                      */
+/* ====================================================== */
+
+const parseNewMatches = function(matches){
+
+}
+
+const parseMatchDone = function(match){
+
+}
+
