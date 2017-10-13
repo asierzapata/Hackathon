1ca2a797d5e7232a62b917a6dfe2cbb83d045799
Eudald Cumalat <eudaldcumalat@gmail.com>
2017-10-13 17:22:18 +0200

    Dir move

--- src/domains/root.js
+++ src/domains/root.js
@@ -1,4 +1,4 @@
-var MatchRouter = require('./match/matchController'); // TODO Change to router when pull is made
+var MatchRouter = require('./match/matchRouter'); 
 var UserRouter = require('./user/userController');  // TODO Change to router when pull is made
 var GameRouter = require('./game/gameRouter');
 var TournamentRouter = require('./tournament/tournamentRouter');
